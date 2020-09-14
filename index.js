78ecce855b9cbb39e5bbe01ddb31832c4f133ee2
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-14 16:42:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,4 +34,6 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction(){
   var myFunckyVar = "FUNKY"
+  return myFunckyVar;
+  
 }
