823e402505d464f652dd24c12936e973cb56e535
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-14 16:24:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function myAnimal() {
 
 function yourAnimal() {
   // The tests expect this function to return `animal` just like the previous function
+  var animal = 'cat'
   // However, you cannot simply modify the existing variable declared on line 1 in the global scope 
   // How can we make sure that this function
   // and the above function both pass?
